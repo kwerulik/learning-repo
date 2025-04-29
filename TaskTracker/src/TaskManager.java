0b3024680a6318a33e349018e1a34fe3b340c8f9
kwerulik <kwerulik@gmail.com>
2025-04-29 12:23:06 +0200

    TaskManager

--- TaskTracker/src/TaskManager.java
+++ TaskTracker/src/TaskManager.java
@@ -1,2 +1,16 @@
+import java.util.*;
+import java.util.*;
+
+
 public class TaskManager {
+    private static final String FILE_NAME = "taks.json";
+    private List<Task> tasks;
+
+     public  TaskManager{
+        tasks = loadTasks();
+     }
+
+     private  List<Task> loadTasks(){
+         List<Task> list = NewArrayList<>();
+    }
 }
