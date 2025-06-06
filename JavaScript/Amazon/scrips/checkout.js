3909a75c079888d4cfcb7e8335b6ee177eabbf36
kwerulik <kwerulik@gmail.com>
2025-06-06 11:59:59 +0200

    lesson 18 - basic of backend

--- JavaScript/Amazon/scrips/checkout.js
+++ JavaScript/Amazon/scrips/checkout.js
@@ -1,7 +1,10 @@
+import { loadProduscts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProduscts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
