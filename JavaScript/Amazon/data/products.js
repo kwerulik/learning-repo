441c13c241ff3d0c973e2d04ca38dbe8ada095a1
kwerulik <kwerulik@gmail.com>
2025-06-07 15:31:35 +0200

    fetch and promises

--- JavaScript/Amazon/data/products.js
+++ JavaScript/Amazon/data/products.js
@@ -72,6 +72,30 @@ const tshirt = new Clothing({
 
 export let products =[];
 
+export function loadProdusctsFetch() {
+  const promise = fetch("https://supersimplebackend.dev/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === "clothing") {
+        return new Clothing(productDetails);
+      }
+
+      return new Product(productDetails);
+    });
+
+    console.log("load products");
+  });
+
+  return promise;
+}
+
+// loadProdusctsFetch().then(() => {
+//   console.log('next step');
+// });
+
 export function loadProduscts(fun) {
   const xhr = new XMLHttpRequest();
 
--- JavaScript/Amazon/scrips/checkout.js
+++ JavaScript/Amazon/scrips/checkout.js
@@ -1,10 +1,44 @@
-import { loadProduscts } from "../data/products.js";
+import { loadProduscts, loadProdusctsFetch } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practise.js';
+import { loadCart } from "../data/cart.js";
 
-loadProduscts(() => {
+
+Promise.all([
+  loadProdusctsFetch(),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  }),
+
+]).then((values) => {
+  console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+// new Promise((resolve) => {
+//   loadProduscts(() => {
+//     resolve('value1');
+//   })
+// }).then((value) => {
+//   console.log(value);     
+//   return new Promise( resolve => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   });
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// });
+
+// loadProduscts(() => {
+//   loadCart(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
